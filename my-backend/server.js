11b3a9dda95236272877dbda10a4072703660808
Pratik <pratiksahane351@gmail,com>
2024-08-15 08:21:00 +0530

    Deleted package-lockjson

--- my-backend/server.js
+++ my-backend/server.js
@@ -19,8 +19,10 @@ const db = mysql.createConnection({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT || 3306,
 });
+
 
 db.connect(err => {
   if (err) {
